345eb6508157efe5278e7f3d25c77e42289ac998
andcost <toscmen.com@gmail.com>
2023-11-09 12:00:12 +0100

    Revert "start of FileReader and Person changes"
    This reverts commit c9b77a9be16fba95f08f9941a99fa7aee8d40940.

--- dai-smtp/src/main/java/dai/smtp/Person.java
+++ dai-smtp/src/main/java/dai/smtp/Person.java
@@ -4,17 +4,24 @@ abstract public class Person {
 
     private final String emailAddress;
 
+<<<<<<< HEAD
     public Person(String address) {
         if (!address.contains("@"))
             throw new RuntimeException("Not a valid email address");
+=======
+    public Person(String address){
+>>>>>>> parent of c9b77a9 (start of FileReader and Person changes)
         emailAddress = address;
     }
 
     public String getEmailAddress() {
         return emailAddress;
     }
+<<<<<<< HEAD
 
     public String getDomain() {
         return emailAddress.split("@")[1];
     }
+=======
+>>>>>>> parent of c9b77a9 (start of FileReader and Person changes)
 }
