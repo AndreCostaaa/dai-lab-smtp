c9b77a9be16fba95f08f9941a99fa7aee8d40940
Amouti <126121965+Amouti@users.noreply.github.com>
2023-11-09 11:09:57 +0100

    start of FileReader and Person changes

--- dai-smtp/src/main/java/dai/smtp/Person.java
+++ dai-smtp/src/main/java/dai/smtp/Person.java
@@ -5,10 +5,18 @@ public class Person{
     final String emailAddress;
 
     public Person(String address){
+        if(!address.contains("@")){
+            throw new RuntimeException();
+        }
         emailAddress = address;
     }
 
     public String getEmailAddress() {
         return emailAddress;
+    }
+
+    public String getDomain(){
+        String[] email = emailAddress.split("@");
+        return email[1];
     }
 }
