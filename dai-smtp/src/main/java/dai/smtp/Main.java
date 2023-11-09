ca549b82753f0f0205e0faa3f4d45cea13c497c9
andcost <toscmen.com@gmail.com>
2023-11-09 12:14:54 +0100

    commented out main

--- dai-smtp/src/main/java/dai/smtp/Main.java
+++ dai-smtp/src/main/java/dai/smtp/Main.java
@@ -2,11 +2,12 @@ package dai.smtp;
 
 public class Main {
     public static void main(String[] args) {
-
-        try (var smtpClient = new SmtpClient("127.0.0.1", 1025)) {
-            smtpClient.sendEmail(new Group());
-        } catch (Exception e) {
-
-        }
+        /*
+         * try (var smtpClient = new SmtpClient("127.0.0.1", 1025)) {
+         * smtpClient.sendEmail();
+         * } catch (Exception e) {
+         * 
+         * }
+         */
     }
 }
