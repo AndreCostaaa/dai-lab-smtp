0651c0c3d2e9cf7678fd9d096e4d5f25c8fa7fa8
andcost <toscmen.com@gmail.com>
2023-11-16 11:49:29 +0100

    added display name

--- dai-smtp/src/main/java/dai/smtp/Person.java
+++ dai-smtp/src/main/java/dai/smtp/Person.java
@@ -3,11 +3,25 @@ package dai.smtp;
 abstract public class Person {
 
     private final String emailAddress;
+    private final String displayName;
 
-    public Person(String address) {
+    public Person(String displayName, String address) {
         if (!address.contains("@"))
             throw new RuntimeException("Not a valid email address");
         emailAddress = address;
+        if (displayName == null) {
+            this.displayName = address.split("@")[0];
+        } else {
+            this.displayName = displayName;
+        }
+    }
+
+    public Person(String address) {
+        this(null, address);
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public String getEmailAddress() {
