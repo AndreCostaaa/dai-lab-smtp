c66ab3cb15fb40c5990c610e27622abfb0b03c5b
Amouti <126121965+Amouti@users.noreply.github.com>
2023-11-23 11:22:53 +0100

    removed jsonParser function

--- dai-smtp/src/main/java/dai/smtp/FileReader.java
+++ dai-smtp/src/main/java/dai/smtp/FileReader.java
@@ -39,28 +39,4 @@ public class FileReader {
             return null;
         }
     }
-
-    public ArrayList<String> jsonParser(File file){
-
-        ArrayList<String> messages = new ArrayList<>();
-        try(JsonReader reader = new JsonReader(new InputStreamReader(
-                new FileInputStream(file), StandardCharsets.UTF_8))){
-            
-            reader.beginArray();
-            while(reader.hasNext()){
-                reader.beginObject();
-                reader.nextName();
-                messages.add(reader.nextString());
-                reader.nextName();
-                messages.add(reader.nextString());
-                reader.endObject();
-            }
-            reader.endArray();
-
-        }
-        catch(IOException e){
-            System.out.println("Error reading Json file");
-        }
-        return messages;
-    }
 }
